96398d29743822d74845ff8d6336ca6f67c893d2
Kasper Neist Christjansen <kasper@astonish.dk>
2017-10-23 16:18:55 +0200

    Added version checker and bumbed up package version

--- check-version.js
+++ check-version.js
@@ -3,6 +3,6 @@ const { engines } = require('./package')
 const version = engines.node;
 
 if (!semver.satisfies(process.version, version)) {
-  console.error('\x1b[31m', `pleasant requires node version ${version}. Not satisfied with current version ${process.version}.`, '\x1b[0m');
+  console.error('\x1b[31m', `pleasant requires node version ${version}. Not satisfied with current version ${process.version}.\n\n`, '\x1b[0m');
   process.exit(1);
 }
